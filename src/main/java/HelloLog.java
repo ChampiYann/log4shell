5697a149d8589742c72c2ce6ebb23a0eff84c3cd
Yann Rosema <yann.rosema@hotmail.com>
2021-12-31 10:53:27 +0100

    sync with bitbucket repo

--- src/main/java/HelloLog.java
+++ src/main/java/HelloLog.java
@@ -1,6 +1,5 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.Level;
 public class HelloLog {
 
     private static final Logger logger = LogManager.getLogger();
@@ -9,10 +8,7 @@ public class HelloLog {
         String userInput = "${jndi:http://localhost/AAAA/BBBB}";
 
         // passing user input into the logger
-        logger.info("Test: "+userInput);
-
-        // %m{nolookups} has no effect for the following line
-        // logger.printf(Level.INFO,"Test: %s", userInput);
+        logger.info("Test: {}", userInput);
     }
 }
 
